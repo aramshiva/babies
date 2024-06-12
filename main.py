e9fc35e340b74dd5f3e329fb9496c902ff5c0d70
Aram <me@aram.sh>
2024-06-11 22:25:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+"""
+NAMES DB
+This script has all names listed on a social security card between 1880 and 2023.
+This data is pulled from the US Social Security Administration's Baby Names from Social Security Card Applications - National Dataset.
+This data can be pulled from this link: https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-data
+This script will insert the data into a MySQL database.
+
+Why? I was bored also why not?
+
+Some things to keep in note:
+- As of 2024 there are around 2,117,219 rows in the database.
+- The data is stored in a folder called "names" in the same directory as this script.
+- Names with 5 or less occurrences with the sex and year are defaulted to 5 by the SSA to protect privacy
+- The data is stored in a MySQL database with the following schema:
+    - name VARCHAR(255),
+    - sex CHAR(1),
+    - amount INT,
+    - year INT
+- The sex is a single character, either "M" or "F" for Male or Female.
+- The year is the year the person was born, NOT registered.
+
+Planned Features (when i get bored again):
+- Add a new column for the state the name was registered/possibly create a new database to store the state data.
+- Create a web interface to search for names and display the data.
+- Graphs! Who doesn't love graphs?
+- An exported db file for those who don't want to set up a MySQL server :D
+"""
 import mysql.connector, os
 from dotenv import load_dotenv
 
@@ -5,11 +32,11 @@ def create_database(db_config):
     conn = mysql.connector.connect(**db_config)
     c = conn.cursor()
     
-    c.execute("USE aramsh_names;")
+    c.execute(f"USE {os.getenv('DATABASE_NAM')};")
 
     # Create table (if dosent exist already )
-    # c.execute('''
-    #     CREATE TABLE IF NOT EXISTS names (
+    # c.execute(f'''
+    #     CREATE TABLE IF NOT EXISTS {names} (
     #         name VARCHAR(255),
     #         sex CHAR(1),
     #         amount INT,
@@ -60,7 +87,6 @@ def main():
     total_rows = 2117219  # you can calculate this automatically if you really wanted
     # as of 2023, the total rows is 2,117,219. This is the total number of rows in all files combined.
     
-    # Create the database and table
     try:
         conn = create_database(db_config)
     except Exception as e:
