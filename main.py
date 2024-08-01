970a2c37d246327988db45543a7de200bcea6a24
Aram <me@aram.sh>
2024-08-01 11:22:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-"""
-NAMES DB
-This script has all names listed on a social security card between 1880 and 2023.
-This data is pulled from the US Social Security Administration's Baby Names from Social Security Card Applications - National Dataset.
-This data can be pulled from this link: https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-national-data
-This script will insert the data into a MySQL database.
-
-Why? I was bored also why not?
-
-Some things to keep in note:
-- As of 2024 there are around 2,117,219 rows in the database.
-- The data is stored in a folder called "names" in the same directory as this script.
-- Names with 5 or less occurrences with the sex and year are defaulted to 5 by the SSA to protect privacy
-- The data is stored in a MySQL database with the following schema:
-    - name VARCHAR(255),
-    - sex CHAR(1),
-    - amount INT,
-    - year INT
-- The sex is a single character, either "M" or "F" for Male or Female.
-- The year is the year the person was born, NOT registered.
-
-Planned Features (when i get bored again):
-- Add a new column for the state the name was registered/possibly create a new database to store the state data.
-- Create a web interface to search for names and display the data.
-- Graphs! Who doesn't love graphs?
-- An exported db file for those who don't want to set up a MySQL server :D
-"""
 import mysql.connector, os
 from dotenv import load_dotenv
 
@@ -34,15 +7,15 @@ def create_database(db_config):
     
     c.execute(f"USE {os.getenv('DATABASE_NAM')};")
 
-    # Create table (if dosent exist already )
-    # c.execute(f'''
-    #     CREATE TABLE IF NOT EXISTS {names} (
-    #         name VARCHAR(255),
-    #         sex CHAR(1),
-    #         amount INT,
-    #         year INT
-    #     )
-    # ''')
+    Create table (if dosent exist already )
+    c.execute(f'''
+        CREATE TABLE IF NOT EXISTS {names} (
+            name VARCHAR(255),
+            sex CHAR(1),
+            amount INT,
+            year INT
+        )
+    ''')
     conn.commit()
     return conn
 
